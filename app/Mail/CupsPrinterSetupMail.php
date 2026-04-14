074ade32b4adb1d3bb4157876fc246089e8c2b27
mamz93us <mamz93@icloud.com>
2026-04-14 17:35:05 +0300

    1212

--- app/Mail/CupsPrinterSetupMail.php
+++ app/Mail/CupsPrinterSetupMail.php
@@ -5,7 +5,7 @@ namespace App\Mail;
 use App\Models\CupsPrinter;
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
-use chillerlan\QRCode\Output\QRMarkupSVG;
+use chillerlan\QRCode\Output\QRGdImagePNG;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
@@ -29,30 +29,36 @@ class CupsPrinterSetupMail extends Mailable
         );
     }
 
-    public function content(): Content
+    public function build(): static
     {
         $domain = \App\Models\Setting::get()->cups_ipp_domain ?? 'localhost';
 
-        // Generate QR code as base64 data URI for the AirPrint profile URL
+        // Generate QR code as PNG base64
         $qrOptions = new QROptions([
-            'outputInterface' => QRMarkupSVG::class,
+            'outputInterface' => QRGdImagePNG::class,
             'scale'           => 10,
             'quietzoneSize'   => 2,
         ]);
 
-        $qrDataUri = (new QRCode($qrOptions))->render($this->airprintUrl);
+        $qrBase64 = (new QRCode($qrOptions))->render($this->airprintUrl);
 
-        return new Content(
-            view: 'emails.cups_printer_setup',
-            with: [
-                'cupsPrinter'   => $this->cupsPrinter,
-                'airprintUrl'   => $this->airprintUrl,
-                'recipientName' => $this->recipientName,
-                'ippAddress'    => $this->cupsPrinter->getIppAddress(),
-                'httpAddress'   => "http://{$domain}:631/printers/{$this->cupsPrinter->queue_name}",
-                'domain'        => $domain,
-                'qrDataUri'     => $qrDataUri,
-            ],
-        );
+        // Strip data URI prefix, decode to raw PNG bytes
+        $qrPngData = base64_decode(preg_replace('#^data:image/png;base64,#', '', $qrBase64));
+
+        // Save temp file, then embed as CID (works in Outlook, Gmail, Apple Mail)
+        $tmpPath = tempnam(sys_get_temp_dir(), 'qr_') . '.png';
+        file_put_contents($tmpPath, $qrPngData);
+
+        $qrCid = $this->embed($tmpPath);
+
+        return $this->view('emails.cups_printer_setup', [
+            'cupsPrinter'   => $this->cupsPrinter,
+            'airprintUrl'   => $this->airprintUrl,
+            'recipientName' => $this->recipientName,
+            'ippAddress'    => $this->cupsPrinter->getIppAddress(),
+            'httpAddress'   => "http://{$domain}:631/printers/{$this->cupsPrinter->queue_name}",
+            'domain'        => $domain,
+            'qrCid'         => $qrCid,
+        ]);
     }
 }
